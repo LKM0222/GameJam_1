70b151f9dd76ab3bc5e4e6f441271e2bc75ad81d
박준희 <dlddukin@gmail.com>
2023-11-04 13:52:23 +0900

    주사위

--- Assets/Script/cubeMove.cs
+++ Assets/Script/cubeMove.cs
@@ -4,46 +4,58 @@ using UnityEngine;
 
 public class cubeMove : MonoBehaviour
 {
+    public GameObject dice1;
+    public GameObject dice2;
 
-    public int speed;
-    public bool isJumpping = false;
+    public float speed;
 
-    private Rigidbody rb;
+    public bool dice1_isJumpping;
+    public bool dice2_isJumpping;
+
+    public Rigidbody dice1_Rb;
+    public Rigidbody dice2_Rb;
+
+    public static cubeMove instance;
+
+    private void Awake()
+    {
+        if (instance == null)
+            instance = this;
+    }
     // Use this for initialization
     void Start()
     {
-        rb = GetComponent<Rigidbody>();
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        moveCube();
+        MoveCube();
     }
 
-    void moveCube()
+    void MoveCube()
     {
-        if (!isJumpping)
+        if (!dice1_isJumpping && !dice2_isJumpping)
         {
             if (Input.GetMouseButtonDown(0))
             {
-                this.isJumpping = true;
-                transform.position = new Vector3(Random.Range(-8f, -11f), 1f, 0);
-                transform.rotation = Quaternion.identity;
-                // rb.AddForce(transform.up * 10);
-                rb.AddTorque(Random.Range(150, 200), Random.Range(150, 200), Random.Range(150, 200));
-                this.GetComponent<Rigidbody>().velocity = Vector3.up * this.speed;
+                dice1_isJumpping = true;
+                dice2_isJumpping = true;
+
+                dice1.transform.position = new Vector3(Random.Range(-1, 1), 1f, 0);
+                dice2.transform.position = new Vector3(Random.Range(-1, 1), 1f, 0);
+
+                dice1.transform.rotation = Quaternion.identity;
+                dice2.transform.rotation = Quaternion.identity;
+
+                dice1_Rb.AddTorque(Random.Range(50, 100), Random.Range(50, 100), Random.Range(50, 100));
+                dice2_Rb.AddTorque(Random.Range(50, 100), Random.Range(50, 100), Random.Range(50, 100));
+
+
+                dice1.GetComponent<Rigidbody>().velocity = Vector3.up * speed;
+                dice2.GetComponent<Rigidbody>().velocity = Vector3.up * speed;
             }
         }
     }
-
-
-    private void OnCollisionEnter(Collision collision)
-    {
-        if (collision.transform.tag == "Ground")
-        {
-            this.isJumpping = false;
-        }
-    }
-
 }
