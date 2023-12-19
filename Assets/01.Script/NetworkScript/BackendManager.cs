62b0f63af1ea5554ebdf5363a7799343c6c1831b
LKM0222 <python03@naver.com>
2023-12-20 00:04:13 +0900

    fix : TitleScene LoginUI수정

--- Assets/01.Script/NetworkScript/BackendManager.cs
+++ Assets/01.Script/NetworkScript/BackendManager.cs
@@ -1,13 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using BackEnd;
 
 public class BackendManager : MonoBehaviour
 {
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        var bro = Backend.Initialize(true);
+        if(bro.IsSuccess()){
+            print("서버 연결 성공" + bro);
+        } else { 
+            print("서버 연결 실패" + bro);
+        }
     }
 
     // Update is called once per frame
