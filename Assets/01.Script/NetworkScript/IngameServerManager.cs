16c929d9402a2d698e765eb56ddd7ffae3a52b2b
LKM0222WIN <python03@naver.com>
2024-01-09 22:36:10 +0900

    feat : 인게임 서버에 접속하는 이벤트 추가. 인게임 서버 접속기능 추가. fix : MatcingRoom Scene에 인게임 서버 데이터 브로드캐스팅 확인용 텍스트 추가.

--- Assets/01.Script/NetworkScript/IngameServerManager.cs
+++ Assets/01.Script/NetworkScript/IngameServerManager.cs
@@ -2,19 +2,32 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using BackEnd;
+using BackEnd.Tcp;
 
 public class IngameServerManager : MonoBehaviour
 {
-    
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    private static IngameServerManager _instance;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    public static IngameServerManager Instance{
+        get{
+            if(_instance == null){
+                _instance = FindObjectOfType(typeof(IngameServerManager)) as IngameServerManager;
+            }
+            return _instance;
+        }
+    }
+    
+    
+    public void JoinGameServer(string _serverAdrr, ushort _serverPort){ //게임서버에 접속하기 위한 함수.
+        bool isReconnect = true;
+        ErrorInfo errorInfo = null;
+        if(Backend.Match.JoinGameServer(_serverAdrr, _serverPort, isReconnect, out errorInfo) == false){ //인게임 서버에 접속 시도.
+            //리턴이 true라면 서버 연결이 된것이 아닌, 소켓 연결만 된것.
+            //인게임 서버 접속 성공확인은 OnSessionJoinInServer 이벤트 핸들러에서 확인. (핸들러가 정상적으로 호출되어야 서버에 접속한것.)
+            //에러정보
+            Debug.Log(errorInfo);
+            return;
+        }
+
     }
 }
