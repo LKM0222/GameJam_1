f402945e4749df1c1d3d3c1a995524ea0a898133
LKM0222 <python03@naver.com>
2024-02-07 15:39:18 +0900

    fix : 모든 카드 나올 수 있게 수정

--- Assets/01.Script/CardManager.cs
+++ Assets/01.Script/CardManager.cs
@@ -480,8 +480,8 @@ public class CardManager : MonoBehaviour
         if (theGM.nowPlayer.cards.Count < 8)
         {
             // 랜덤하게 카드번호를 추출
-            // Card newCard = theGM.cards[UnityEngine.Random.Range(0, theGM.cards.Length)];
-            Card newCard = theGM.cards[UnityEngine.Random.Range(1, 2)];
+            Card newCard = theGM.cards[UnityEngine.Random.Range(0, theGM.cards.Length)];
+            // Card newCard = theGM.cards[UnityEngine.Random.Range(1, 2)];//Test
 
             // 팻말 아래 카드리스트에 복제하고 플레이어의 카드 목록에 추가함
 
