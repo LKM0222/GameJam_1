ab1afc4e0a2e6d8cabe8059b89c1be69c8546996
yen-world <dlddukin@gmail.com>
2024-04-04 17:02:24 +0900

    refactor: CardManager 리팩토링

--- Assets/01.Script/CardManager.cs
+++ Assets/01.Script/CardManager.cs
@@ -5,9 +5,7 @@ using UnityEngine;
 
 public class CardManager : MonoBehaviour
 {
-    GameManager theGM;
-    TileManager theTile;
-    TurnSignScript theTSI;
+    TurnSignScript theTurnSign;
     DiceSystem theDice;
 
     // 카드의 정보
@@ -32,16 +30,14 @@ public class CardManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        theGM = FindObjectOfType<GameManager>();
-        theTile = FindObjectOfType<TileManager>();
-        theTSI = FindObjectOfType<TurnSignScript>();
+        theTurnSign = FindObjectOfType<TurnSignScript>();
         theDice = FindObjectOfType<DiceSystem>();
     }
 
     // 획득한 카드에 마우스를 올려놓았을 때
     private void OnMouseEnter()
     {
-        if (theTSI.cursorPos == 1 && !theDice.isDrag)
+        if (theTurnSign.cursorPos == 1 && !theDice.isDrag)
         {
             AudioManager.Instance.Play("SelectCard_Sound");
 
@@ -54,7 +50,7 @@ public class CardManager : MonoBehaviour
     // 획득한 카드에 마우스를 올렸다가 떨어트렸을 때
     private void OnMouseExit()
     {
-        if (theTSI.cursorPos == 1 && !theDice.isDrag)
+        if (theTurnSign.cursorPos == 1 && !theDice.isDrag)
         {
             this.transform.localScale = new Vector3(7f, 7f, 1f);
             this.transform.position += Vector3.down * upPos;
@@ -65,11 +61,11 @@ public class CardManager : MonoBehaviour
     // 획득한 카드를 클릭해서 사용할 때
     private void OnMouseDown()
     {
-        if (theTSI.cursorPos == 1 && GameManager.Instance.myCharactor.myTurn)
+        if (theTurnSign.cursorPos == 1 && GameManager.Instance.myCharactor.myTurn)
         {
             AudioManager.Instance.Play("UseCard_Sound");
 
-            if (cardInfo.cardCode == 1 && !theGM.nowPlayer.highSpeedFlag)
+            if (cardInfo.cardCode == 1 && !GameManager.Instance.nowPlayer.highSpeedFlag)
             {
                 CardClickData cData = new(1, GameManager.Instance.nowPlayer.playerId);
                 string jsonData = JsonUtility.ToJson(cData);
@@ -79,7 +75,7 @@ public class CardManager : MonoBehaviour
                 DestroyCard();
             }
 
-            else if (cardInfo.cardCode == 2 && !theGM.nowPlayer.invisibleFlag)
+            else if (cardInfo.cardCode == 2 && !GameManager.Instance.nowPlayer.invisibleFlag)
             {
                 CardClickData cData = new(2, GameManager.Instance.nowPlayer.playerId);
                 string jsonData = JsonUtility.ToJson(cData);
@@ -89,7 +85,7 @@ public class CardManager : MonoBehaviour
                 DestroyCard();
             }
 
-            else if (cardInfo.cardCode == 3 && !theGM.nowPlayer.biggerFlag)
+            else if (cardInfo.cardCode == 3 && !GameManager.Instance.nowPlayer.biggerFlag)
             {
                 CardClickData cData = new(3, GameManager.Instance.nowPlayer.playerId);
                 string jsonData = JsonUtility.ToJson(cData);
@@ -99,38 +95,38 @@ public class CardManager : MonoBehaviour
                 DestroyCard();
             }
 
-            else if (cardInfo.cardCode == 4 && !theGM.nowPlayer.lowerDiceFlag)
+            else if (cardInfo.cardCode == 4 && !GameManager.Instance.nowPlayer.lowerDiceFlag)
             {
                 CardClickData cData = new(4, GameManager.Instance.nowPlayer.playerId);
                 string jsonData = JsonUtility.ToJson(cData);
                 byte[] data = ParsingManager.Instance.ParsingSendData(ParsingType.CardClick, jsonData);
                 Backend.Match.SendDataToInGameRoom(data);
 
-                theGM.nowPlayer.lowerDiceFlag = true;
-                theGM.nowPlayer.higherDiceFlag = false;
+                GameManager.Instance.nowPlayer.lowerDiceFlag = true;
+                GameManager.Instance.nowPlayer.higherDiceFlag = false;
 
                 DestroyCard();
             }
 
-            else if (cardInfo.cardCode == 5 && !theGM.nowPlayer.higherDiceFlag)
+            else if (cardInfo.cardCode == 5 && !GameManager.Instance.nowPlayer.higherDiceFlag)
             {
                 CardClickData cData = new(5, GameManager.Instance.nowPlayer.playerId);
                 string jsonData = JsonUtility.ToJson(cData);
                 byte[] data = ParsingManager.Instance.ParsingSendData(ParsingType.CardClick, jsonData);
                 Backend.Match.SendDataToInGameRoom(data);
 
-                theGM.nowPlayer.higherDiceFlag = true;
-                theGM.nowPlayer.lowerDiceFlag = false;
+                GameManager.Instance.nowPlayer.higherDiceFlag = true;
+                GameManager.Instance.nowPlayer.lowerDiceFlag = false;
 
                 DestroyCard();
             }
 
-            else if (cardInfo.cardCode == 7 && !theGM.nowPlayer.laserFlag && theGM.laserComplete)
+            else if (cardInfo.cardCode == 7 && !GameManager.Instance.nowPlayer.laserFlag && GameManager.Instance.laserComplete)
             {
-                for (int i = 0; i < theTile.tiles.Length; i++)
+                for (int i = 0; i < TileManager.Instance.tiles.Length; i++)
                 {
                     // 상대방 소유의 타일 체크
-                    if (theTile.tiles[i].ownPlayer == theGM.nowPlayer.againstPlayer.playerId)
+                    if (TileManager.Instance.tiles[i].ownPlayer == GameManager.Instance.nowPlayer.againstPlayer.playerId)
                     {
                         CardClickData cData = new(7, GameManager.Instance.nowPlayer.playerId);
                         string jsonData = JsonUtility.ToJson(cData);
@@ -155,23 +151,23 @@ public class CardManager : MonoBehaviour
 
     public void HighSpeedMove()
     {
-        theGM.nowPlayer.diceNum = theGM.nowPlayer.diceNum * 2;
-        theGM.nowPlayer.moveSpeed = 6f;
+        GameManager.Instance.nowPlayer.diceNum = GameManager.Instance.nowPlayer.diceNum * 2;
+        GameManager.Instance.nowPlayer.moveSpeed = 6f;
         highMoveParticle.gameObject.SetActive(true);
         highMoveParticle.Play();
     }
 
     public void EndHighSpeedMove()
     {
-        theGM.nowPlayer.highSpeedFlag = false;
-        theGM.nowPlayer.moveSpeed = 4f;
+        GameManager.Instance.nowPlayer.highSpeedFlag = false;
+        GameManager.Instance.nowPlayer.moveSpeed = 4f;
         highMoveParticle.Stop();
         highMoveParticle.gameObject.SetActive(false);
     }
 
     public IEnumerator InvisibleThief()
     {
-        if (theGM.nowPlayer.cards.Count < 8)
+        if (GameManager.Instance.nowPlayer.cards.Count < 8)
         {
             byte[] sendData = ParsingManager.Instance.ParsingSendData(ParsingType.InvisibleThief, "");
             Backend.Match.SendDataToInGameRoom(sendData);
@@ -182,45 +178,45 @@ public class CardManager : MonoBehaviour
             AudioManager.Instance.Play("InvisibleThief_Sound");
 
             // 천사꼬꼬 카드를 가져왔다면 
-            if (theGM.nowPlayer.againstPlayer.cards[GameManager.Instance.invisibleCardNum] == theGM.cards[5])
+            if (GameManager.Instance.nowPlayer.againstPlayer.cards[GameManager.Instance.invisibleCardNum] == GameManager.Instance.cards[5])
             {
-                theGM.nowPlayer.exemptionFlag = true;
-                theGM.nowPlayer.againstPlayer.exemptionFlag = false;
+                GameManager.Instance.nowPlayer.exemptionFlag = true;
+                GameManager.Instance.nowPlayer.againstPlayer.exemptionFlag = false;
             }
 
             // 파티클 실행
             InvisibleParticle.gameObject.SetActive(true);
-            InvisibleParticle.transform.position = theGM.nowPlayer.transform.position;
+            InvisibleParticle.transform.position = GameManager.Instance.nowPlayer.transform.position;
             InvisibleParticle.Play();
 
             // 플레이어 카드 리스트 업데이트
-            theGM.nowPlayer.cards.Add(theGM.nowPlayer.againstPlayer.cards[GameManager.Instance.invisibleCardNum]);
-            theGM.nowPlayer.againstPlayer.cards.RemoveAt(GameManager.Instance.invisibleCardNum);
-            theGM.CardListUpdate();
+            GameManager.Instance.nowPlayer.cards.Add(GameManager.Instance.nowPlayer.againstPlayer.cards[GameManager.Instance.invisibleCardNum]);
+            GameManager.Instance.nowPlayer.againstPlayer.cards.RemoveAt(GameManager.Instance.invisibleCardNum);
+            GameManager.Instance.CardListUpdate();
 
-            GameObject dCard = theGM.nowPlayer.againstPlayer.cardParent.GetChild(0).gameObject;
-            dCard.transform.SetParent(theGM.nowPlayer.cardParent);
-            dCard.GetComponent<SpriteRenderer>().sprite = theGM.nowPlayer.cardPrefab.GetComponent<SpriteRenderer>().sprite;
+            GameObject dCard = GameManager.Instance.nowPlayer.againstPlayer.cardParent.GetChild(0).gameObject;
+            dCard.transform.SetParent(GameManager.Instance.nowPlayer.cardParent);
+            dCard.GetComponent<SpriteRenderer>().sprite = GameManager.Instance.nowPlayer.cardPrefab.GetComponent<SpriteRenderer>().sprite;
         }
     }
 
     public void EndInvisibleThief()
     {
-        theGM.nowPlayer.invisibleFlag = false;
+        GameManager.Instance.nowPlayer.invisibleFlag = false;
         InvisibleParticle.Stop();
         InvisibleParticle.gameObject.SetActive(false);
-        StartCoroutine(theGM.nowPlayer.SetPlayerTransparency("Visible"));
+        StartCoroutine(GameManager.Instance.nowPlayer.SetPlayerTransparency("Visible"));
     }
 
     public IEnumerator TollExemption()
     {
-        for (int i = 0; i < theGM.nowPlayer.cards.Count; i++)
+        for (int i = 0; i < GameManager.Instance.nowPlayer.cards.Count; i++)
         {
             // 천사꼬꼬 카드를 찾아서 삭제
-            if (theGM.nowPlayer.cards[i] == theGM.cards[5])
+            if (GameManager.Instance.nowPlayer.cards[i] == GameManager.Instance.cards[5])
             {
-                theGM.nowPlayer.cards.RemoveAt(i);
-                Destroy(theGM.nowPlayer.cardParent.GetChild(0).gameObject);
+                GameManager.Instance.nowPlayer.cards.RemoveAt(i);
+                Destroy(GameManager.Instance.nowPlayer.cardParent.GetChild(0).gameObject);
                 break;
             }
         }
@@ -228,30 +224,30 @@ public class CardManager : MonoBehaviour
         AudioManager.Instance.Play("TollExemption_Sound");
 
         // 파티클 실행
-        exemptionParticle.transform.position = theGM.nowPlayer.transform.position;
+        exemptionParticle.transform.position = GameManager.Instance.nowPlayer.transform.position;
         exemptionParticle.gameObject.SetActive(true);
         exemptionParticle.Play();
         yield return new WaitForSeconds(1f);
         exemptionParticle.gameObject.SetActive(false);
 
-        theGM.nowPlayer.exemptionFlag = false;
+        GameManager.Instance.nowPlayer.exemptionFlag = false;
         yield return null;
     }
 
     public IEnumerator BiggerCoroutine()
     {
-        theGM.nowPlayer.biggerFlag = false;
+        GameManager.Instance.nowPlayer.biggerFlag = false;
 
         // 상대방 소유의 타일이라면 거대화 꼬꼬 효과 발동
-        if (theGM.nowPlayer.nowTile.ownPlayer != -1 && theGM.nowPlayer.playerId != theGM.nowPlayer.nowTile.ownPlayer)
+        if (GameManager.Instance.nowPlayer.nowTile.ownPlayer != -1 && GameManager.Instance.nowPlayer.playerId != GameManager.Instance.nowPlayer.nowTile.ownPlayer)
         {
-            Color buildingColor = theGM.nowPlayer.nowTile.buildingImg.GetComponent<SpriteRenderer>().color;
-            Color tileColor = theGM.nowPlayer.nowTile.signImg.GetComponent<SpriteRenderer>().color;
+            Color buildingColor = GameManager.Instance.nowPlayer.nowTile.buildingImg.GetComponent<SpriteRenderer>().color;
+            Color tileColor = GameManager.Instance.nowPlayer.nowTile.signImg.GetComponent<SpriteRenderer>().color;
 
             AudioManager.Instance.Play("BuildingDestroy_Sound");
 
             destroyParticle.gameObject.SetActive(true);
-            destroyParticle.transform.position = theGM.nowPlayer.nowTile.transform.GetChild(0).position;
+            destroyParticle.transform.position = GameManager.Instance.nowPlayer.nowTile.transform.GetChild(0).position;
             destroyParticle.Play();
 
             // 타일과 건물의 색상을 점차 투명하게 변경
@@ -260,8 +256,8 @@ public class CardManager : MonoBehaviour
                 buildingColor.a -= 0.02f;
                 tileColor.a -= 0.02f;
 
-                theGM.nowPlayer.nowTile.buildingImg.GetComponent<SpriteRenderer>().color = buildingColor;
-                theGM.nowPlayer.nowTile.signImg.GetComponent<SpriteRenderer>().color = tileColor;
+                GameManager.Instance.nowPlayer.nowTile.buildingImg.GetComponent<SpriteRenderer>().color = buildingColor;
+                GameManager.Instance.nowPlayer.nowTile.signImg.GetComponent<SpriteRenderer>().color = tileColor;
 
                 yield return new WaitForSeconds(0.02f);
             }
@@ -269,14 +265,14 @@ public class CardManager : MonoBehaviour
             destroyParticle.gameObject.SetActive(false);
 
             // 타일의 소유주 변경
-            theGM.nowPlayer.nowTile.ownPlayer = -1;
-            theGM.nowPlayer.nowTile.building = theGM.buildings[0];
+            GameManager.Instance.nowPlayer.nowTile.ownPlayer = -1;
+            GameManager.Instance.nowPlayer.nowTile.building = GameManager.Instance.buildings[0];
 
             // 타일과 건물의 색상 원상복구
             buildingColor.a = 1f;
             tileColor.a = 1f;
-            theGM.nowPlayer.nowTile.buildingImg.GetComponent<SpriteRenderer>().color = buildingColor;
-            theGM.nowPlayer.nowTile.signImg.GetComponent<SpriteRenderer>().color = tileColor;
+            GameManager.Instance.nowPlayer.nowTile.buildingImg.GetComponent<SpriteRenderer>().color = buildingColor;
+            GameManager.Instance.nowPlayer.nowTile.signImg.GetComponent<SpriteRenderer>().color = tileColor;
         }
 
         biggerComplete = true;
@@ -284,32 +280,32 @@ public class CardManager : MonoBehaviour
 
     public IEnumerator SelectLaserCoroutine()
     {
-        theGM.laserComplete = false;
+        GameManager.Instance.laserComplete = false;
 
-        theGM.nowPlayer.blackBackground.SetActive(true);
+        GameManager.Instance.nowPlayer.blackBackground.SetActive(true);
 
         isSelectingLaser = true;
 
         // 상대방 소유의 타일을 클릭 가능하도록 변경
-        for (int i = 0; i < theTile.tiles.Length; i++)
+        for (int i = 0; i < TileManager.Instance.tiles.Length; i++)
         {
-            if (theGM.nowPlayer.playerId != theTile.tiles[i].ownPlayer && theTile.tiles[i].ownPlayer != -1)
-                theTile.tiles[i].canTileSelect = true;
+            if (GameManager.Instance.nowPlayer.playerId != TileManager.Instance.tiles[i].ownPlayer && TileManager.Instance.tiles[i].ownPlayer != -1)
+                TileManager.Instance.tiles[i].canTileSelect = true;
         }
 
-        yield return new WaitUntil(() => theGM.seletedTile != null);
+        yield return new WaitUntil(() => GameManager.Instance.seletedTile != null);
 
         isSelectingLaser = false;
 
         // 클릭 이후 다시 클릭이 불가능하도록 변경
-        for (int i = 0; i < theTile.tiles.Length; i++)
+        for (int i = 0; i < TileManager.Instance.tiles.Length; i++)
         {
-            theTile.tiles[i].canTileSelect = false;
+            TileManager.Instance.tiles[i].canTileSelect = false;
         }
 
-        theGM.nowPlayer.blackBackground.SetActive(false);
+        GameManager.Instance.nowPlayer.blackBackground.SetActive(false);
 
-        LaserData laserData = new(true, theGM.seletedTile.name);
+        LaserData laserData = new(true, GameManager.Instance.seletedTile.name);
         string jsondata = JsonUtility.ToJson(laserData);
         byte[] sendData = ParsingManager.Instance.ParsingSendData(ParsingType.Laser, jsondata);
         Backend.Match.SendDataToInGameRoom(sendData);
@@ -362,11 +358,11 @@ public class CardManager : MonoBehaviour
 
     public IEnumerator ShowGetCard(int _randomNum)
     {
-        GameObject card = Instantiate(theGM.onlyCardImg, Vector3.zero, Quaternion.identity, theGM.showCardObject.transform);
+        GameObject card = Instantiate(GameManager.Instance.onlyCardImg, Vector3.zero, Quaternion.identity, GameManager.Instance.showCardObject.transform);
         card.transform.localPosition = new Vector3(0f, 0f, 0f);
         card.transform.localScale = new Vector3(20f, 20f, 20f);
 
-        card.GetComponent<SpriteRenderer>().sprite = theGM.cards[_randomNum].cardImg;
+        card.GetComponent<SpriteRenderer>().sprite = GameManager.Instance.cards[_randomNum].cardImg;
 
         yield return new WaitForSeconds(3f);
 
@@ -378,10 +374,10 @@ public class CardManager : MonoBehaviour
     {
         isGetCard = false;
 
-        if (theGM.nowPlayer.cards.Count < 8)
+        if (GameManager.Instance.nowPlayer.cards.Count < 8)
         {
-            int randomNum = Random.Range(0, theGM.cards.Length);
-            Card newCard = theGM.cards[randomNum];
+            int randomNum = Random.Range(0, GameManager.Instance.cards.Length);
+            Card newCard = GameManager.Instance.cards[randomNum];
 
             CardData cardData = new(newCard);
             string jsonData = JsonUtility.ToJson(cardData);
@@ -397,11 +393,11 @@ public class CardManager : MonoBehaviour
             isShowCard = false;
 
             // 통행료 면제 카드를 획득했을 때
-            if (newCard == theGM.cards[5])
+            if (newCard == GameManager.Instance.cards[5])
             {
                 AudioManager.Instance.Play("TollExemption_Sound");
 
-                exemptionParticle.transform.position = theGM.nowPlayer.transform.position;
+                exemptionParticle.transform.position = GameManager.Instance.nowPlayer.transform.position;
                 exemptionParticle.gameObject.SetActive(true);
                 exemptionParticle.Play();
 
