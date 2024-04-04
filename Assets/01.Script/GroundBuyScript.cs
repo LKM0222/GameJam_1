d35ea10a3b61c3abb27d06e573517daa085a7b29
yen-world <dlddukin@gmail.com>
2024-04-04 21:36:25 +0900

    refactor: GroundBuyScript 리팩토링

--- Assets/01.Script/GroundBuyScript.cs
+++ Assets/01.Script/GroundBuyScript.cs
@@ -2,72 +2,23 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-// 토지 구매 관리 스크립트(건물 X)
+// 토지 구매 스크립트
 public class GroundBuyScript : MonoBehaviour
 {
-    GameManager theGM;
-    public PurchaseSystem thePS;
-
-    // [SerializeField] PlayerManager thePlayer;
-
+    public PurchaseSystem thePurchase;
     public GameObject purchaseUI;
 
-    // 땅을 구매하면 건물 구매창을 띄우기 위한 플래그
-    public bool groundBuyFlag;
-
-    // public bool groundBuyFlag;
-    // Start is called before the first frame update
-    void Start()
-    {
-        theGM = FindObjectOfType<GameManager>();
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        //구매 버튼을 눌렀다면 땅의 소유주 변경 후, 땅에 건물 올리기 위한 UI활성화
-        if (groundBuyFlag)
-        {
-            groundBuyFlag = false; //재반복 막기 위해 false;
-            theGM.nowPlayer.nowTile.price = 50;
-
-            theGM.nowPlayer.nowTile.ownPlayer = theGM.nowPlayer.playerId; //땅을 샀으니, 소유주 변경
-            this.transform.parent.gameObject.SetActive(false);//현재 UI 닫기
-
-            if (theGM.nowPlayer.playerMoney >= 50)
-            {
-                // 건물 구매 UI 출력시 항상 첫번째 건물을 띄워주게 변경
-                thePS.cur = 1;
-                thePS.buildingImg.sprite = theGM.buildings[1].buildingImg.building_front;
-                thePS.buildingText.text = theGM.buildings[1].buildingName;
-
-                purchaseUI.SetActive(true); //UI활성화
-            }
-            else
-            {
-                theGM.NextTurnFunc();
-            }
-        }
-        // //구매 버튼을 눌렀다면 땅의 소유주 변경 후, 땅에 건물 올리기 위한 UI활성화
-        // if (groundBuyFlag)
-        // {
-        //     // 건물 구매 UI 출력시 항상 첫번째 건물을 띄워주게 변경
-
-        // }
-    }
     public void GroundBuy()
     {
-        print("groundbuy func");
-        thePS.cur = 1;
-        thePS.buildingImg.sprite = theGM.buildings[1].buildingImg.building_front;
-        thePS.buildingText.text = theGM.buildings[1].buildingName;
+        // 타일 소유주 변경 및 타일 가격 설정
+        GameManager.Instance.nowPlayer.nowTile.ownPlayer = GameManager.Instance.nowPlayer.playerId;
+        GameManager.Instance.nowPlayer.nowTile.price = 50;
+        this.transform.parent.gameObject.SetActive(false);
 
-        purchaseUI.SetActive(true); //UI활성화
-        theGM.nowPlayer.nowTile.ownPlayer = theGM.nowPlayer.playerId; //땅을 샀으니, 소유주 변경
-        // theGM.buyedTiles.Add(theGM.nowPlayer.nowTile.gameObject); //구매한 타일을 저장
-        this.transform.parent.gameObject.SetActive(false);//현재 UI 닫기
-
-        theGM.nowPlayer.nowTile.price = 50;
+        // 건물 구매 UI에 띄워줄 이미지, 건물 이름 초기화
+        thePurchase.cur = 1;
+        thePurchase.buildingImg.sprite = GameManager.Instance.buildings[1].buildingImg.building_front;
+        thePurchase.buildingText.text = GameManager.Instance.buildings[1].buildingName;
+        purchaseUI.SetActive(true);
     }
 }
-
