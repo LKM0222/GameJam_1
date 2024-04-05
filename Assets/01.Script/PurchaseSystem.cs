768a73d04feeb0bed035d36ef4671c299973c38c
yen-world <dlddukin@gmail.com>
2024-04-05 13:51:43 +0900

    refactor: PurchaseSystem 리팩토링

--- Assets/01.Script/PurchaseSystem.cs
+++ Assets/01.Script/PurchaseSystem.cs
@@ -3,46 +3,28 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-// 건물 구매 관리 스크립트
+// 건물 구매 스크립트
 public class PurchaseSystem : MonoBehaviour
 {
-    GameManager theGM;
-
-    // 구매창 UI에 띄울 건물의 이름
     public Text buildingText;
-    // 구매창 UI에 띄울 건물의 이미지
     public Image buildingImg;
 
-    // 어떤 건물을 선택중인지 순서로 접근하는 변수
     public int cur = 1;
 
-    // Start is called before the first frame update
     void Start()
     {
-        theGM = FindObjectOfType<GameManager>();
-        buildingImg.sprite = theGM.buildings[1].buildingImg.building_front;
-        buildingText.text = theGM.buildings[1].buildingName;
+        buildingImg.sprite = GameManager.Instance.buildings[1].buildingImg.building_front;
+        buildingText.text = GameManager.Instance.buildings[1].buildingName;
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-
-    }
-
-    // 빌딩 건설 함수
+    // 건물 건설 함수
     public void BuildingPurchase()
     {
-        theGM.nowPlayer.nowTile.building = theGM.buildings[cur]; //타일의 건물 정보를 현재 커서에서 선택한 건물로 변경
+        GameManager.Instance.nowPlayer.nowTile.building = GameManager.Instance.buildings[cur];
+        GameManager.Instance.nowPlayer.nowTile.price = GameManager.Instance.buildings[cur].toll;
+        GameManager.Instance.nowPlayer.playerMoney -= 50;
+        GameManager.Instance.SetFloatingText(GameManager.Instance.nowPlayer, 50, false);
 
-        this.transform.parent.gameObject.SetActive(false);//모든 작업 끝났으니 현재 UI종료
-
-        theGM.nowPlayer.buildingCount += 1;
-        theGM.nowPlayer.playerMoney -= 50;
-        theGM.SetFloatingText(theGM.nowPlayer, 50, false);
-        theGM.nowPlayer.nowTile.price = theGM.buildings[cur].toll;
-
-        // theGM.UIFlag = false; // UI를 끄기 위해 UIFlag False;
-        // theGM.NextTurnFunc(); //next turn 호출
+        this.transform.parent.gameObject.SetActive(false);
     }
 }
